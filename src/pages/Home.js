d38feb0a6771f0185863113ad62920d000ccc5c0
Sauter <gabriel.andrade.sauter@gmail.com>
2023-09-15 14:31:38 -0300

    Initial Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <p>
+          Hello!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
