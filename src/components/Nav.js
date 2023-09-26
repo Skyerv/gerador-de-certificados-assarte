f59e96f28cf3790ea5e042f936e6e305e95bb393
Sauter <gabriel.andrade.sauter@gmail.com>
2023-09-26 15:18:56 -0300

    Added Certificate page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,11 @@ import './Nav.css';
 
 const Nav = () => {
   return (
-    <div>
-      <h1>Feira Cultural ASSARTE 2023</h1>
+    <div className='navbar'>
+      <div className='nav-container'>
+        <img src="/images/ASSARTELogo.png" alt="ASSARTE logo" className='nav-image'/>
+        <h1>Feira Cultural ASSARTE 2023</h1>
+      </div>
     </div>
   );
 };
