b63ef55b63bef4bdc46fcf7c6defaa83cd756ebf
Sauter <gabriel.andrade.sauter@gmail.com>
2023-10-16 14:01:30 -0300

    Made navigations links on navbar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,15 +1,45 @@
 import React from "react";
 import NavCSS from "./Nav.module.css";
+import { Link, useLocation } from "react-router-dom";
 
 const Nav = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className={NavCSS.navContainer}>
-      <h1 className={NavCSS.navTitle}>Feira Cultural ASSARTE 2023</h1>
       <img
         src="/images/ASSARTELogo.png"
         alt="ASSARTE logo"
         className={NavCSS.navImage}
       />
+
+      <h1 className={NavCSS.navTitle}>Feira Cultural ASSARTE</h1>
+
+      <ul className={NavCSS.navUl}>
+        <li>
+          <Link to="/" className={pathname === "/" ? NavCSS.selected : ""}>
+            Página Inicial
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/procurar-certificados"
+            className={
+              pathname === "/procurar-certificados" ? NavCSS.selected : ""
+            }
+          >
+            Certificados
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/login"
+            className={pathname === "/login" ? NavCSS.selected : ""}
+          >
+            Sou Professor
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
